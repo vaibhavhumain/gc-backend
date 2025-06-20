3448e81d0beee36dba620e9630361b5bd291e56f
vaibhav <codweird5@gmail.com>
2025-06-20 12:31:22 +0530

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json()); 
 
 app.use('/api/blogs', blogRoutes);
